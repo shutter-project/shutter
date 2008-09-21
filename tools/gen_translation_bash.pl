1ff2e9227649592ba140638866d219e4421a2fde
unknown <mario.kemper@googlemail.com>
2008-09-21 15:32:24 +0200

    see changelog

---
+++ tools/gen_translation_bash.pl
@@ -0,0 +1,54 @@
+#! /usr/bin/perl
+
+#Copyright (C) Mario Kemper 2008 <mario.kemper@googlemail.com> Mi, 09 Apr 2008 22:58:09 +0200 
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use utf8;
+use strict;
+use warnings;
+
+#read files to translate
+open(LIST, "./to_translate_bash") or die $!;
+my @translate_files = <LIST>;
+close LIST or die $!;
+
+	#open files to translate
+foreach my $file (@translate_files){
+	chomp $file;
+	
+	#folder? then add files included
+	if(-d $file){
+		my @new_files = <$file/*>;
+		push(@translate_files, @new_files);
+		next;	
+	}
+	next unless (-T $file); #textfile??
+	
+	open(FILE, $file) or die $!." :$file";
+	$file =~ s{^.*/}{};
+	open(FILE_TMP, ">>./translate_tmp.sh") or die $!;	
+	print "Preparing file $file\n";
+	while (<FILE>){
+		chomp;
+		print FILE_TMP $_."\n";		
+	}
+	close FILE or die $!;
+	close FILE_TMP or die $!;
+	print "Done file $file\n";
+}
+my $time = time;
+system("xgettext ./translate_tmp.sh -o ./gscrot_plugins_bash$time.pot");		
+unlink("./translate_tmp.sh");
+
