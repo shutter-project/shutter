8fde901956e331006fdfb53afcd2ac63f2c5f99e
Mario Kemper <mario.kemper@googlemail.com>
2010-09-03 12:55:51 +0200

    implemented new actions for keybindings

--- share/shutter/resources/modules/Shutter/App/HelperFunctions.pm
+++ share/shutter/resources/modules/Shutter/App/HelperFunctions.pm
@@ -163,10 +163,14 @@ sub usage {
 	print "shutter [options]\n";
 	print "Available options:\n\n"
 		. "Capture:\n"
-		. "--full (starts shutter and takes a full screen screenshot directly)\n"
 		. "--selection (starts shutter in selection mode)\n"
+		. "--desktop (starts shutter and takes a full screen screenshot directly)\n"
 		. "--window (starts shutter in window selection mode)\n"
-		. "--section (starts shutter in section selection mode)\n\n"
+		. "--aswindow (no text)\n"
+		. "--section (starts shutter in section selection mode)\n"
+		. "--menu (starts shutter in menu selection mode)\n"
+		. "--tooltip (starts shutter in tooltip selection mode)\n"
+		. "--web (no text)\n\n"
 		.
 
 		"Application:\n"
@@ -174,6 +178,7 @@ sub usage {
 		. "--clear_cache (clears cache, e.g. installed plugins, at startup)\n"
 		. "--debug (prints a lot of debugging information to STDOUT)\n"
 		. "--disable_systray (disable systray icon)\n"
+		. "--version (displays version information)\n"
 		. "--help (displays this help)\n";
 
 	return TRUE;
