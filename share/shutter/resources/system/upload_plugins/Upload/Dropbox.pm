1fa6907b4e42df6e212aea10a7e7bf153535be9f
Maike Lutz <maike@linux-sio8.site>
2011-10-09 15:34:40 +0200

    Updated Dropbox.pm

--- share/shutter/resources/system/upload_plugins/Upload/Dropbox.pm
+++ share/shutter/resources/system/upload_plugins/Upload/Dropbox.pm
@@ -75,11 +75,11 @@ sub init {
 	use JSON;
 	use URI::Escape qw(uri_escape_utf8);
 	use File::Basename qw(dirname basename);
-	use Path::Class;
+	use File::Spec;
 
 	$self->{_box} = undef;
 	$self->{_config} = { };
-	$self->{_config_file} = file( ($ENV{'HOME'}, '.dropbox-api-config') );
+	$self->{_config_file} = File::Spec->catfile( $ENV{'HOME'}, '.dropbox-api-config' );
 	
 	$self->connect;
 }
@@ -92,10 +92,13 @@ sub connect {
 			$self->{_config} = decode_json($self->{_config_file}->slurp);
 			$self->{_config}->{key} = 'fwsv9z8slaw0c0q';
 			$self->{_config}->{secret} = 'hsxflivocvav6ag';
-			$self->{_config}->{callback_url} = '';			
+			$self->{_config}->{callback_url} = '';						
 			$self->{_box} = Net::Dropbox::API->new($self->{_config});
 			$self->{_box}->context('dropbox');
 		}else{
+			$self->{_config}->{key} = 'fwsv9z8slaw0c0q';
+			$self->{_config}->{secret} = 'hsxflivocvav6ag';
+			$self->{_config}->{callback_url} = '';				
 			return $self->setup;
 		}
 	};
@@ -145,6 +148,7 @@ sub setup {
 			$self->{_config}->{access_secret} = $self->{_box}->access_secret;
 			$self->{_config}->{key} = 'fwsv9z8slaw0c0q';
 			$self->{_config}->{secret} = 'hsxflivocvav6ag';
+			$self->{_config}->{callback_url} = '';
 			
 			print $self->{_config}->{access_token}, "\n";
 			print $self->{_config}->{access_secret}, "\n";
